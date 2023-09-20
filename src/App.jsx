5d4524c4fa7fe8d6463afd3d74aba090f354339b
saknouche <developpeur.web90@gmail.com>
2023-09-20 10:06:20 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Post from "./components/Post.jsx"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center">Projet front</h1>
+      <Post/>
+    </>
+  )
+}
+
+export default App
