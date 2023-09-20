269dc921e4eb874c99eb6e84677e51079ce3a84f
saknouche <developpeur.web90@gmail.com>
2023-09-20 13:50:18 +0200

    Creation du state posts et map dessus puis passage d'un post en props du parent vers l'enfant

--- src/components/timeline/TimeLine.jsx
+++ src/components/timeline/TimeLine.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
-import './TimeLine.css'
+import React, { useState } from 'react';
+import './TimeLine.css';
 import Suggestions from './Suggestions.jsx';
 import Post from './posts/Post.jsx';
 
 const TimeLine = () => {
-    return (
-        <div className='d-flex'>
-            <div className="timeline_left col-9 justify-content-center">
-                <div className="timeline_posts">
-                    <Post/>
-                </div>
+   const [posts, setPosts] = useState([
+      {
+         user: 'redian_',
+         postImage:
+            'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+         likes: 54,
+         timestamp: '1d',
+      },
+      {
+         user: 'johndoe',
+         postImage:
+            'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80',
+         likes: 432,
+         timestamp: '2d',
+      },
+      {
+         user: 'mariussss',
+         postImage:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png',
+         likes: 140,
+         timestamp: '12h',
+      },
+      {
+         user: 'kobee_18',
+         postImage:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGCAaQ5u1TMTij5ELPWi5-VPtlSqELw-R6lj0EpYmNcGt56kOQaCokzS0IK81MOSphlkw&usqp=CAU',
+         likes: 14,
+         timestamp: '30min',
+      },
+   ]);
+
+   return (
+      <div className='d-flex'>
+         <div className='timeline_left col-9 justify-content-center'>
+            <div className='timeline_posts'>
+               {posts.map((post, index) => (
+                  <Post key={index} post={post} />
+               ))}
             </div>
-            <div className="timeline_right col-3">
-                <Suggestions/>
-            </div>
-        </div>
-    );
+         </div>
+         <div className='timeline_right col-3'>
+            <Suggestions />
+         </div>
+      </div>
+   );
 };
 
 export default TimeLine;
--- src/components/timeline/posts/Post.jsx
+++ src/components/timeline/posts/Post.jsx
@@ -1,41 +1,44 @@
-import React from 'react';
-import './Post.css'
+import React, { useState } from 'react';
+import './Post.css';
 import { Avatar } from '@mui/material';
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import TelegramIcon from "@mui/icons-material/Telegram";
-import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 
-const Post = () => {
+const Post = ({post}) => {
 
-
-    return (
-        <div className='post'>
-            <div className="post_header">
-                <div className="post_headerAuthor">
-                    <Avatar>S</Avatar>
-                    SaDev <span> 12h</span>
-                </div>
-                <MoreHorizIcon/>
+   return (
+      <div className='post'>
+         <div className='post_header'>
+            <div className='post_headerAuthor'>
+               <Avatar>{post.user.charAt(0).toUpperCase()}</Avatar>
+               {post.user}<span>{post.timestamp}</span>
             </div>
-            <div className="post_image">
-                <img src="https://images.unsplash.com/photo-1695131402412-e342cbccae53?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2942&q=80" alt="" />
+            <MoreHorizIcon />
+         </div>
+         <div className='post_image'>
+            <img
+               src={post.postImage}
+               alt={`image-${post.user}`}
+            />
+         </div>
+         <div className='post_footer'>
+            <div className='post_footerIcons'>
+               <div className='post_iconsMain'>
+                  <FavoriteBorderIcon className='postIcon' />
+                  <ChatBubbleOutlineIcon className='postIcon' />
+                  <TelegramIcon className='postIcon' />
+               </div>
+               <div className='post_iconsSave'>
+                  <BookmarkBorderIcon className='postIcon' />
+               </div>
             </div>
-            <div className="post_footer">
-                <div className="post_footerIcons">
-                    <div className="post_iconsMain">
-                        <FavoriteBorderIcon className='postIcon'/>
-                        <ChatBubbleOutlineIcon className='postIcon'/>
-                        <TelegramIcon className='postIcon'/>
-                    </div>
-                    <div className="post_iconsSave">
-                        <BookmarkBorderIcon className='postIcon'/>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+            Liked by {post.likes} people
+         </div>
+      </div>
+   );
 };
 
 export default Post;
